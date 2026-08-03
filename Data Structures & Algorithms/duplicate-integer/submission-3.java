3c05fea1cf47a0472b11e0b0fde9e1ae876ad991
Devansh Singh <devanshsin2020@gmail.com>
2026-08-03 23:41:48 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int n=nums.length;
+        int duplicate=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    duplicate = 1;
+                    break;
+                }
+                
+            }
+            if(duplicate == 1) break;
+        }
+        return duplicate == 1;
+        
+    }
+}
